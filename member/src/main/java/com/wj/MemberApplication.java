cf3e3b2066cd7e6930f297c81766f2b646de0a07
wj <1724031211@qq.com>
2023-07-08 11:05:33 +0800

    新增member模块

--- member/src/main/java/com/wj/MemberApplication.java
+++ member/src/main/java/com/wj/MemberApplication.java
@@ -1,13 +1,11 @@
-package com.wj.train;
+package com.wj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainApplication {
-
+public class MemberApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TrainApplication.class, args);
+        SpringApplication.run(MemberApplication.class, args);
     }
-
 }
