9cac7a5cccc00038602251fb2902b2138a46751b
wj <1724031211@qq.com>
2023-07-26 15:45:09 +0800

    使用nacos对sentinel实现限流规则持久化

--- business/src/main/java/com/wj/train/business/controller/ConfirmOrderController.java
+++ business/src/main/java/com/wj/train/business/controller/ConfirmOrderController.java
@@ -1,19 +1,26 @@
 package com.wj.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.wj.train.business.req.ConfirmOrderQueryReq;
 import com.wj.train.business.req.ConfirmOrderSaveReq;
 import com.wj.train.business.resp.ConfirmOrderQueryResp;
 import com.wj.train.business.service.ConfirmOrderService;
 import com.wj.train.common.context.LocalContext;
+import com.wj.train.common.exception.BusinessException;
 import com.wj.train.common.resp.CommonResp;
 import com.wj.train.common.resp.PageResp;
 import com.wj.train.common.utils.RespUtil;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import static com.wj.train.common.exception.BusinessExceptionEnum.BUSINESS_TOO_MANY_PEOPLE;
 
 @RestController
 @RequestMapping("/confirm-order")
+@Slf4j
 public class ConfirmOrderController {
 
     @Resource
@@ -32,11 +39,20 @@ public class ConfirmOrderController {
     }
 
     @PostMapping("/do")
+    @SentinelResource(value = "confirmOrderController",blockHandler = "confirmOrderBlockHandler")
     public CommonResp<Object> confirmOrder(@RequestBody ConfirmOrderSaveReq confirmOrderSaveReq) {
         Long memberId = LocalContext.getMemberId();
         confirmOrderSaveReq.setMemberId(memberId);
         confirmOrderService.confirmOrder(confirmOrderSaveReq);
         return RespUtil.success(true);
+    }
+
+    /**
+     * 限流降级方法
+     */
+    public CommonResp<Object> confirmOrderBlockHandler(ConfirmOrderSaveReq confirmOrderSaveReq, BlockException blockException) {
+        log.info("接口/confirm-order/do 触发限流降级");
+        throw new BusinessException(BUSINESS_TOO_MANY_PEOPLE);
     }
 
 }
